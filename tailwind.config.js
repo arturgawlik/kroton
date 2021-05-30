c1fef570ea5af6aef594d523f14418b64367733f
arturgawlik <artur.gawlik@icloud.com>
2021-05-30 22:30:16 +0200

    chore: tailwind enable purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: [
+      './src/**/*.html',
+      './src/**/*.scss'
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
