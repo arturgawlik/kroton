96285f33de2ba65a851328ff5bc17eea5f286586
arturgawlik <artur.gawlik@icloud.com>
2021-06-09 23:39:44 +0200

    fix(module-menu): mobile view

--- libs/web/shell/ui/module-menu/src/lib/module-menu.component.ts
+++ libs/web/shell/ui/module-menu/src/lib/module-menu.component.ts
@@ -4,21 +4,21 @@ import { faClock, faTasks, faFire, faUserFriends } from '@fortawesome/free-solid
 @Component({
   selector: 'kroton-module-menu',
   template: `
-    <div class="flex flex-wrap bg-white mt-14 kroton-max-width mx-auto p-1 justify-center sm:justify-start bg-blue-400 rounded shadow">
-      <kroton-button krotonClasses="m-2 p-1 bg-green-400 text-white w-36 ">
+    <div class="flex flex-col sm:flex-row bg-white mt-14 kroton-max-width mx-auto p-4 justify-center sm:justify-start bg-blue-400 rounded shadow">
+      <kroton-button krotonClasses="p-1 bg-green-400 text-white w-full sm:w-36 my-1 sm:m-1">
         <fa-icon [icon]="faClock"></fa-icon>
         <span class="ml-2">Work Time</span>
       </kroton-button>
-      <kroton-button krotonDisabled="true" krotonClasses="m-2 p-1 bg-pink-400 cursor-not-allowed text-white w-36">
+      <kroton-button krotonDisabled="true" krotonClasses="p-1 bg-pink-400 cursor-not-allowed text-white w-full sm:w-36 my-1 sm:m-1">
         <fa-icon [icon]="faTasks"></fa-icon>
         <span class="ml-2">Tasks</span>
       </kroton-button>
-      <kroton-button krotonDisabled="true" krotonClasses="m-2 p-1 bg-red-400 cursor-not-allowed text-white w-36">
+      <kroton-button krotonDisabled="true" krotonClasses="p-1 bg-red-400 cursor-not-allowed text-white w-full sm:w-36 my-1 sm:m-1">
         <fa-icon [icon]="faUserFriends"></fa-icon>
         <span class="ml-2">Team</span>
       </kroton-button>
-      <div class="lg:ml-auto">
-        <kroton-button krotonClasses="m-2 p-1 text-white border-white">
+      <div class="sm:ml-auto">
+        <kroton-button krotonClasses="p-1 text-white border-white w-full sm:w-auto my-1 sm:m-1">
           <fa-icon [icon]="faFire"></fa-icon>
           <span class="ml-2">Ask for new feature</span>
         </kroton-button>
